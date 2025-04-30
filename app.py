3ad7fed1cca56c3faa080db2ca5b552edd532406
JuniRed <102496020+JuniRed@users.noreply.github.com>
2025-04-30 13:57:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def compare_images(img1, img2):
 def compare():
     data = request.get_json()
 
-    document_base64 = data.get("document_image", "")
-    reference_base64 = data.get("reference_signature", "")
+    document_base64 = data.get("image1", "")
+    reference_base64 = data.get("image2", "")
 
     if not document_base64 or not reference_base64:
         return jsonify({"error": "Both document_image and reference_signature are required"}), 400
